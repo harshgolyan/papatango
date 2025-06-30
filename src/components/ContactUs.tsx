996919652d3f441156d64e13b3a400d30d0a3886
harshgolyan <harshgolyan308@gmail.com>
2025-06-30 14:38:38 +0530

    contect us created

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,7 +2,16 @@
 function ContactUs() {
   return (
     <>
-        <div className="h-screen w-full bg-[#D1D1C6]"></div>
+        <div className="flex flex-col gap-6 my-12 py-20 justify-center items-center w-full bg-[#D1D1C6]">
+            <div className="text-4xl font-parastoo">( 01 )</div>
+            <div className="text-2xl font-semibold font-roboto">Guides par la creativite</div>
+            <div className="max-w-4xl text-center text-6xl font-extralight leading-14 font-parastoo pt-10">
+                Chez Papatango, la creativite est bien plus qu''une simple expression artistique, c''est une force strategique capable de transformer des ambitions en reussite. Nous decortiquons les idees, analy'ysons les aspirations, et fusionnons votre vision avec les attentes de votre cible. 
+            </div>
+            <div>
+                <button className="px-14 py-5 bg-black text-2xl font-semibold text-white mt-10">Contact Us</button>
+            </div>
+        </div>
     </>
   )
 }
